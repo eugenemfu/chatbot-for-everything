0cbf5e98308a719265a7af768f782f1a686ac67d
Eugene <sahevg@gmail.com>
2021-06-16 21:08:57 +0300

    update

--- src/handlers/DomainHandler.py
+++ src/handlers/DomainHandler.py
@@ -6,18 +6,32 @@ from definitions import BOT_STATE
 class DomainHandler(StateHandler):
     def __init__(self, state_id: int = BOT_STATE.DOMAIN_RECOGNITION):
         super().__init__(state_id)
+        self.keywords = dict()
+        self.keywords['weather'] = [
+            'weather',
+            'погода',
+            'погоду',
+        ]
 
     def generate_answer(self, msg: str) -> Tuple[int, str]:
         msg = msg.lower()
-        if msg == "kirill":
-            next_state = BOT_STATE.KIRILL_DOMAIN
-        elif msg == "dasha":
-            next_state = BOT_STATE.DASHA_DOMAIN
-        elif msg == "weather":
-            next_state = BOT_STATE.WEATHER
-        elif msg == "intro":
-            next_state = BOT_STATE.INTRO
-        else:
-            next_state = self.state_id
+        words = msg.split()
+        weather, kirill, dasha, goodbye = False
+        for word in words:
+            if word in self.keywords['weather']:
+                weather = True
+        if weather + kirill + dasha + goodbye != 1:
+            return self.state_id, 'Переформулируй, пожалуйста, я не понял'
+
         ans = "Whatever"
+
+        if kirill:
+            next_state = BOT_STATE.KIRILL_DOMAIN
+        elif dasha:
+            next_state = BOT_STATE.DASHA_DOMAIN
+        elif weather:
+
+        elif goodbye:
+            next_state = BOT_STATE.INTRO
+
         return next_state, ans
