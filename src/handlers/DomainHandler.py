58620409edd882893427687e2ee175ff962441b1
Eugene <sahevg@gmail.com>
2021-06-16 21:33:44 +0300

    оно живое

--- src/handlers/DomainHandler.py
+++ src/handlers/DomainHandler.py
@@ -7,34 +7,49 @@ class DomainHandler(StateHandler):
     def __init__(self, handlers: Dict[int, StateHandler], state_id: int = BOT_STATE.DOMAIN_RECOGNITION):
         super().__init__(state_id)
         self.handlers = handlers
-        self.keywords = dict()
-        self.keywords['weather'] = [
-            'weather',
-            'погода',
-            'погоду',
-        ]
+        self.keywords = {
+            'weather': [
+                'weather',
+                'погода',
+                'погоду',
+            ],
+            'kirill': [
+                'аудитория',
+            ],
+            'dasha': [
+                'вино',
+            ],
+            'goodbye': [
+                'пока',
+            ]
+        }
 
     def generate_answer(self, msg: str) -> Tuple[int, str]:
         msg = msg.lower()
         words = msg.split()
-        weather, kirill, dasha, goodbye = False
+        weather, kirill, dasha, goodbye = [False] * 4
         for word in words:
             if word in self.keywords['weather']:
                 weather = True
+            if word in self.keywords['kirill']:
+                kirill = True
+            if word in self.keywords['dasha']:
+                dasha = True
+            if word in self.keywords['goodbye']:
+                goodbye = True
         if weather + kirill + dasha + goodbye != 1:
             return self.state_id, 'Переформулируй, пожалуйста, я не понял'
 
         ans = "Whatever"
+        next_state = self.state_id
 
-        if msg == "kirill":
+        if kirill:
             next_state, ans = self.handlers[BOT_STATE.KIRILL_DOMAIN.value].generate_answer(msg)
-        elif msg == "dasha":
+        elif dasha:
             next_state, ans = self.handlers[BOT_STATE.DASHA_DOMAIN.value].generate_answer(msg)
-        elif msg == "weather":
+        elif weather:
             next_state, ans = self.handlers[BOT_STATE.WEATHER.value].generate_answer(msg)
-        elif msg == "intro":
-            next_state, ans = self.handlers[BOT_STATE.INTRO.value].generate_answer(msg)
-        else:
-            ans, next_state = "Whatever", self.state_id
+        elif goodbye:
+            next_state, ans = BOT_STATE.INTRO, 'Пока!'
 
         return next_state, ans
