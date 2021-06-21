54a9269ee5053558e26fec013b43e20d4ec9e50f
diatlova <dari.diatlova@gmail.com>
2021-06-21 17:47:01 +0300

    refactoring

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 from pymystem3 import Mystem
 from string import punctuation
 
@@ -11,7 +10,6 @@ def lemmatize(msg: str) -> List[str]:
     tokens = word_tokenize(msg)
     # words without punctuation
     filtered_tokens = [w for w in tokens if not w.lower() in punctuation]
-    # lemmatizer = WordNetLemmatizer()
     lemmatizer = Mystem()
     # words in infinitive form
     lemmas = [str(lemmatizer.lemmatize(w)[0]) for w in filtered_tokens]
@@ -19,6 +17,10 @@ def lemmatize(msg: str) -> List[str]:
 
 
 def lemmatize_list(words: List) -> List:
-    # lemmatizer = WordNetLemmatizer()
     lemmatizer = Mystem()
     return [str(lemmatizer.lemmatize(w.lower())[0]) for w in words]
+
+
+def translate_list_to_str(list_msg: List) -> str:
+    str_msg = ", ".join(str(x) for x in list_msg[:-1])
+    return str_msg
