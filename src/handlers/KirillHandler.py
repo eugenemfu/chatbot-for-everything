d1d6d0d16cfdff795f3a82abf1cfadd0643c6564
Kirill.Semin <north7311@gmail.com>
2021-06-30 10:01:23 +0300

    Improved generation

--- src/handlers/KirillHandler.py
+++ src/handlers/KirillHandler.py
@@ -8,7 +8,7 @@ from src.handlers.handlers import StateHandler
 from definitions import BOT_STATE, SpecialTokens, TOKENIZER_PATH, CHGK_MODEL_PATH, ROOT_DIR
 
 
-class GPT2SberSmall(torch.nn.Module):
+class GPT2SberAbstract(torch.nn.Module):
     def __init__(self, model_dir: str, tokenizer_path: str, device: torch.device):
         super().__init__()
         self.model: GPT2LMHeadModel = GPT2LMHeadModel.from_pretrained(model_dir)
@@ -28,16 +28,52 @@ class GPT2SberSmall(torch.nn.Module):
         return self.model.forward(*args, **kwargs)
 
     @torch.no_grad()
-    def generate(self, context: str, max_length: int, beam_size: int):
+    def generate(
+        self,
+        context: str,
+        max_length: int = 100,
+        beam_size: int = 5,
+        sampling: bool = True,
+        top_k: int = 50,
+        top_p: float = 0.9,
+    ):
         input_ids = self._pre_example(context)
 
+        if not sampling:
+            generated_text = self._generate_beam(input_ids, max_length, beam_size)
+        else:
+            generated_text = self._generate_sampling(input_ids, max_length, top_k, top_p)
+
+        generated_text = self._post_example(generated_text)
+
+        return generated_text
+
+    def _generate_beam(self, input_ids, max_length: int, beam_size: int) -> str:
         greedy_output = self.model.generate(
             input_ids, max_length=max_length, beam_size=beam_size, no_repeat_ngram_size=2, early_stopping=True
         )
         generated_output = self.tokenizer.decode(greedy_output[0], skip_special_tokens=True)
-        generated_output = self._post_example(generated_output)
 
         return generated_output
+
+    def _generate_sampling(self, input_ids, max_length: int, top_k: int = 50, top_p: float = 0.9):
+        sampling_output = self.model.generate(
+            input_ids, do_sample=True, max_length=max_length, top_k=top_k, top_p=top_p
+        )
+        generated_output = self.tokenizer.decode(sampling_output[0], skip_special_tokens=True)
+
+        return generated_output
+
+    def _pre_example(self, example) -> torch.LongTensor:
+        ...
+
+    def _post_example(self, generated_text) -> str:
+        ...
+
+
+class GPT2SberSimple(GPT2SberAbstract):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def _pre_example(self, example) -> torch.LongTensor:
         example = SpecialTokens.BOS.value + " " + example
@@ -61,7 +97,7 @@ class KirillHandler(StateHandler):
         super().__init__(state_id)
         self.asked_for_context = False
 
-        self.model = GPT2SberSmall(os.path.join(ROOT_DIR, CHGK_MODEL_PATH), TOKENIZER_PATH, torch.device("cpu"))
+        self.model = GPT2SberSimple(os.path.join(ROOT_DIR, CHGK_MODEL_PATH), TOKENIZER_PATH, torch.device("cpu"))
         self.model.eval()
 
         self.generation_params = {
